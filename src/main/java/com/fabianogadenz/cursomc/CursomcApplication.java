f48a9145d9444e543f872869b6f2953707b572c3
Fabiano Gadenz <fabianomgadenz@gmail.com>
2017-12-11 17:22:54 -0200

    criando operacao de instanciacao

--- src/main/java/com/fabianogadenz/cursomc/CursomcApplication.java
+++ src/main/java/com/fabianogadenz/cursomc/CursomcApplication.java
@@ -2,9 +2,10 @@ package com.fabianogadenz.cursomc;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+ import org.springframework.boot.SpringApplication;
+ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.fabianogadenz.cursomc.domain.Categoria;
 import com.fabianogadenz.cursomc.repositories.CategoriaRepository;
@@ -12,6 +13,7 @@ import com.fabianogadenz.cursomc.repositories.CategoriaRepository;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
+	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
 	public static void main(String[] args) {
@@ -19,10 +21,11 @@ public class CursomcApplication implements CommandLineRunner{
 	}
 
 	@Override
-	public void run(String... arg0) throws Exception {
-		Categoria cat1 = new Categoria(null, "Informatica");
-		Categoria cat2 = new Categoria(null, "Escritorio");
-		
-		categoriaRepository.save(Arrays.asList(cat1, cat2));
-	}
+	 	public void run(String... args) throws Exception {
+	 		
+	 		Categoria cat1 = new Categoria(null, "Informática");
+	 		Categoria cat2 = new Categoria(null, "Escritório");
+	 				
+	 		categoriaRepository.save(Arrays.asList(cat1, cat2));
+	 	}
 }
--- src/main/java/com/fabianogadenz/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/fabianogadenz/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,6 @@
 
 package com.fabianogadenz.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
