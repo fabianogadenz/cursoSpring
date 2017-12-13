69b95186ba158949b452dd65c28fedace0a4fec2
Fabiano Gadenz <fabianomgadenz@gmail.com>
2017-12-13 14:25:21 -0200

    ajustes no endpoint /categorias/{id}

--- src/main/java/com/fabianogadenz/cursomc/services/CategoriaService.java
+++ src/main/java/com/fabianogadenz/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.fabianogadenz.cursomc.domain.Categoria;
 import com.fabianogadenz.cursomc.repositories.CategoriaRepository;
+import com.fabianogadenz.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,9 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado ID"+ id + "tipo: "+ Categoria.class.getName());
+		}
 		return obj;
 	}
 
